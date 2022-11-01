ae9cc33a131266b5e704a0f29030f1bd9b467030
Natalia <lisnatalia2002.lnmc@gmail.com>
2022-11-01 19:38:37 +0100

    first commit

---
+++ warmUp.py
@@ -0,0 +1,33 @@
+#ABCs
+def getSum(A: int, B: int, C: int) -> int:
+  return A+B+C
+# 0(n)
+
+
+#All Wrong
+def getWrongAnswers(N: int, C: str) -> str:
+    c = " "
+    for i in C:
+        if i == "A":
+            c = c + "B"
+        else:
+            c = c + "A"
+        
+    print(c)
+            
+getWrongAnswers(3, 'ABA')
+# O(n)
+
+
+#Battleship
+from typing import List
+def getHitProbability(R: int, C: int, G: List[List[int]]) -> float:
+  a = 0
+  for i in range(R):
+    for j in range(C):
+      if G[i][j] == 1:
+        a+=1
+
+
+  return a/(R*C)
+# 0(n²) 
